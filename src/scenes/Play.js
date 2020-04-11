04e08ad5f619247639b5443039cb629871c0691f
mnguy161 <mnguy161@ucsc.edu>
2020-04-11 16:12:53 -0700

    Rocket Implementation

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,12 +4,37 @@ class Play extends Phaser.Scene {
     }
 
     create () {
+        //place tile sprite 
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0,0);
+
         //white rectangle borders 
         this.add.rectangle(5, 5, 630, 32, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(5, 443, 630, 32, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(5, 5, 32, 455, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(603, 5, 32, 455, 0xFFFFFF).setOrigin(0,0);
+
         //green UI background
         this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0,0);
+
+        //rocket (p1)
+        this.p1Rocket = new Rocket(this, game.config.width/2 - 8, 431, 'rocket').setScale(0.5,0.5).setOrigin(0,0);
+        
+        //define keys
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
+    }
+
+    update () {
+        this.starfield.tilePositionX -= 4;
+        this.p1Rocket.update();
+    }
+
+    preload () {
+        //load images / tile sprites
+        this.load.image('rocket','./assets/rocket.png');
+        this.load.image('spaceship','./assets/spaceship.png');
+        this.load.image('starfield','./assets/starfield.png');
     }
 }
