037cad951f00f5e6fdad41b5e2b31c9bea200cfa
mnguy161 <mnguy161@ucsc.edu>
2020-04-12 06:08:12 -0700

    Spaceship and Score implemented

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -34,6 +34,11 @@ class Rocket extends Phaser.GameObjects.Sprite {
         }
     }
 
+    //reset rocket to "ground"
+    reset () {
+        this.isFiring = false; 
+        this.y = 431; 
+    }
 
 
 
